980b57859e03cbfdfb9dc842a370eeca7bb68aba
qualiu <qualiu@microsoft.com>
2019-08-07 16:25:32 +0800

    Solve abnormal PATH value case.

--- disable-exe-in-PATH.bat
+++ disable-exe-in-PATH.bat
@@ -24,15 +24,18 @@ set ninExe=%~dp0nin.exe
 if not exist %ninExe% powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri https://github.com/qualiu/msr/blob/master/tools/nin.exe?raw=true -OutFile %~dp0nin.exe"
 
 :: Display files with exe pattern %1
-%msrExe% -l -f "%~1" --wt --sz -p "%PATH%" 2>nul
+%msrExe% -l -f "%~1" --wt --sz 2>nul -M -p "%PATH%"
 if %ERRORLEVEL% EQU 0 exit /b 0
 
+for /f "tokens=*" %%a in ('msr -t "[^\.\w-]+$" -o ";" -aPAC -z "%PATH%" ^| msr -x "\;" -o ";" -aPAC') do set "PATH=%%a"
 set "tmpPATH=%PATH%"
+
 for /f "tokens=*" %%a in ('%msrExe% -l -f "%~1" -PAC 2^>nul -p "%PATH%" ^| %ninExe% nul "^([a-z]+.+?)[\\/][^\\/]*$" -iuPAC') do (
     :: echo Will remove in PATH: %%a
-    for /f "tokens=*" %%b in ('%msrExe% -z "!tmpPATH!" -t "\\*\s*;\s*" -o "\n" -aPAC ^| %msrExe% --nx %%a -i -PAC ^| %msrExe% -S -t "[\r\n]+\s*(\S+)" -o ";$1" -aPAC ^| %msrExe% -S -t "\s+$" -o "" -aPAC') do (
+    for /f "tokens=*" %%b in ('%msrExe% -z "!tmpPATH!" -t "\\*\s*;\s*" -o "\n" -aPAC ^| %msrExe% --nx %%a -i -PAC ^| %msrExe% -S -t "[\r\n]+(\S+)" -o ";$1" -aPAC ^| %msrExe% -S -t "\s+$" -o "" -aPAC') do (
         set "tmpPATH=%%b"
     )
 )
 
 EndLocal & set "PATH=%tmpPATH%"
+exit /b 0
