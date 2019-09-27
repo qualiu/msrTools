44195a010be84f14b5f6239206f91b969c92c7c5
qualiu <qualiu@microsoft.com>
2019-09-27 19:15:57 +0800

    Improve disable-exe-in-PATH.bat + detect-and-init-visual-studio-env.bat to support clear existing environment variables of different VS.

--- detect-and-init-visual-studio-env.bat
+++ detect-and-init-visual-studio-env.bat
@@ -1,57 +1,70 @@
 :: ==================================================================================================
-:: Auto detect and use latest Visual Studio from environment variables like: VS150COMNTOOLS
-::    VS110COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\
-::    VS120COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\
-::    VS140COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\
-::    VS150COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\
-:: If not found variables like VS150COMNTOOLS,
-::  Then find the Visual Studio link from start menu.
+:: Auto detect latest or specific Visual Studio and initialize the environment.
+:: Support clearing previous different environment variables.
+::
+:: If not found variables like VS150COMNTOOLS, Then find the Visual Studio link from start menu.
 ::
 :: If need the older version on your machine, please manually initialize the older one at first.
+::
+:: More/Dependency scripts: https://github.com/qualiu/msrTools
 :: ==================================================================================================
 @echo off
 
-( where msbuild.exe 2>nul >nul && where devenv.exe 2>nul >nul ) && exit /b 0
+where msr.exe /q || if not exist %~dp0\msr.exe powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri https://github.com/qualiu/msr/blob/master/tools/msr.exe?raw=true -OutFile %~dp0\msr.exe"
+where msr.exe /q || set "PATH=%~dp0;%PATH%"
 
-where msr.exe 2>nul >nul || if not exist %~dp0\msr.exe powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri https://github.com/qualiu/msr/blob/master/tools/msr.exe?raw=true -OutFile %~dp0\msr.exe"
-where msr.exe 2>nul >nul || set "PATH=%~dp0;%PATH%"
+where nin.exe /q || if not exist %~dp0\nin.exe powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri https://github.com/qualiu/msr/blob/master/tools/nin.exe?raw=true -OutFile %~dp0\nin.exe"
+where nin.exe /q || set "PATH=%~dp0;%PATH%"
 
-msr -z "NotFirstArg%~1" -t "^NotFirstArg(-h|--help|/\?)$" > nul
-if %ERRORLEVEL% NEQ 0 (
-    echo Usage: %0  [MustContainWordsForVisualStudio]
+msr -z "NotFirstArg%~1" -t "^NotFirstArg(-h|--help|/\?)$" >nul || (
+    echo Usage: %0   [MustContainWordsForVisualStudio]
+    echo Example: %0
     echo Example: %0  2019
     echo Example: %0  2017
     exit /b -1
 )
 
-if not "%~1" == "" set MatchVSArgs=-ix "%~1"
+if not "%~1" == "" (
+    set MatchVSArgs=-ix "%~1"
+    set MatchOtherVSArgs=--nx "%~1"
+    call :Clear_Other_VS_Env || ( call :Clear_Tmp_Envs "%~dp0%~nx0" & exit /b -1 )
+)
+
+call :DetectAndCheckMatchWords && ( call :Clear_Tmp_Envs "%~dp0%~nx0" & exit /b 0 )
 
 :: Get path like: C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Visual Studio 2017\Visual Studio Tools\Developer Command Prompt for VS 2017.lnk
 for /f "tokens=*" %%a in ('msr -rp "%ProgramData%\Microsoft\Windows\Start Menu\Programs" -d "Visual Studio" -l -f "Develop.*Promp.*\.lnk$" -PAC ^| msr %MatchVSArgs% -s "[^\\]+?(\d+)[^\\]*$" -PAC -T 1') do (
-    for /f "tokens=*" %%b in ('powershell -Command "(New-Object -COM WScript.Shell).CreateShortcut('%%a').Arguments" ^| msr -t "^\s*/\w+\s*" -o "" -PAC') do (
+    echo %%a | msr -aPA -e .+
+    for /f "tokens=*" %%b in ('powershell -Command "(New-Object -COM WScript.Shell).CreateShortcut('%%a').Arguments" ^| msr %MatchVSArgs% -t "^\s*/\w+\s*" -o "" -PAC') do (
        :: Cannot direct call %%b , for VS2015 extra double quotes: ""C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat""
-       for /f "tokens=*"  %%c in ('echo %%~b') do call "%%~c"
+       for /f "tokens=*"  %%c in ('echo %%~b') do (
+          echo %%~c
+          call "%%~c"
+       )
     )
 )
 
-( where msbuild.exe 2>nul >nul && where devenv.exe 2>nul >nul ) && exit /b 0
-
+call :DetectAndCheckMatchWords && ( call :Clear_Tmp_Envs "%~dp0%~nx0" & exit /b 0 )
 
 :: To avoid missing non-English link file name like: VS 2019的开发人员命令提示符.lnk
 for /f "tokens=*" %%a in ('dir "%ProgramData%\Microsoft\Windows\Start Menu\Programs"\v*s*20*.lnk /S /B /O:D ^| nin nul "^(.+?)[^\\]+$" %MatchVSArgs% -uPAC -T 1') do (
-    for /f "tokens=*" %%b in ('powershell -Command "[IO.directory]::GetFiles('%%a', '*.lnk') | ForEach-Object { (New-Object -COM WScript.Shell).CreateShortcut(\"$_\") }" ^| msr -it "\.(cmd|bat)" -PAC ^| msr -it ".*?\W([A-Z]:\\\w+.*?\.(bat|cmd)).*" -o "$1" -aPAC -s "(\d{2,}+\S*)" -T 1') do call "%%~b"
+    for /f "tokens=*" %%b in ('powershell -Command "[IO.directory]::GetFiles('%%a', '*.lnk') | ForEach-Object { (New-Object -COM WScript.Shell).CreateShortcut(\"$_\") }" ^| msr -it "\.(cmd|bat)" -PAC ^| msr -it ".*?\W([A-Z]:\\\w+.*?\.(bat|cmd)).*" -o "$1" -aPAC -s "(\d{2,}+\S*)" -T 1') do (
+        echo %%~b
+        call "%%~b"
+    )
 )
 
-( where msbuild.exe 2>nul >nul && where devenv.exe 2>nul >nul ) && exit /b 0
+call :DetectAndCheckMatchWords && ( call :Clear_Tmp_Envs "%~dp0%~nx0" & exit /b 0 )
 
 
 for /f "tokens=*" %%a in ('set VS ^| msr -it "^vs\d+\w+Tools=(.+?)\\?\s*$" -s "^(\w+)" -o "$1" -PAC -T 1') do (
     for /f "tokens=*" %%b in ('msr -p "%%a" -f "^(VsDevCmd|vsvar\w+)\.(cmd|bat)$" -l -PAC -H 1') do (
+        echo %%b
         call "%%b"
     )
 )
 
-( where msbuild.exe 2>nul >nul && where devenv.exe 2>nul >nul ) && exit /b 0
+call :DetectAndCheckMatchWords && ( call :Clear_Tmp_Envs "%~dp0%~nx0" & exit /b 0 )
 
 echo %~nx0: Not found Visual Studio environment with devenv.exe or msbuild.exe | msr -aPA -x "%~nx0" -it "(Not found.+)"
 
@@ -59,4 +72,37 @@ for /f "tokens=*" %%a in ('where msr.exe ^| msr -H 1 -PAC') do set "msrPath=%%a"
 echo Please check whether Visual Studio exists in 'Start Menu' ^(Use %msrPath% if not found msr.exe^) by command below: | msr -aPA -e "Start Menu|(\S+.exe)" -t "Please.*?in"
 echo msr -rp "%ProgramData%\Microsoft\Windows\Start Menu\Programs" -d "Visual Studio" -f "Develop.*\.lnk$" -l | msr -aPA -t "\s+(-\w+)" -e ".+"
 
-exit /b -1
+call :Clear_Tmp_Envs "%~dp0%~nx0" & exit /b -1
+
+:DetectAndCheckMatchWords
+    set /a matchVsCount=0
+    (where msbuild.exe 2>nul | msr -t "20\d+" %MatchVSArgs% -aPM >nul ) || set /a matchVsCount+=1
+    (where devenv.exe 2>nul | msr -t "20\d+" %MatchVSArgs% -aPM >nul ) || set /a matchVsCount+=1
+    if %matchVsCount% EQU 2 exit /b 0
+    exit /b 1
+
+:Clear_Other_VS_Env
+    set /a otherVsCount=0
+    (where msbuild.exe 2>nul | msr -t "20\d+" %MatchOtherVSArgs% -aPM >nul ) || set /a otherVsCount+=1
+    (where devenv.exe 2>nul | msr -t "20\d+" %MatchOtherVSArgs% -aPM >nul ) || set /a otherVsCount+=1
+
+    if %otherVsCount% GTR 0 (
+        for /f "tokens=*" %%a in ('set ^| msr -it "^(DevEnvDir|CommandPromptType|VSINSTALLDIR|\w*VSCMD_\w+|VC\w+Dir|_*DOTNET\w+|INCLUDE|LIB|LIBPATH|VisualStudioVersion|NETFXSDKDir|VCTool\w+)=.*" -o "\1" -PAC') do set "%%a="
+        echo call %~dp0\disable-exe-in-PATH.bat "^(msbuild|devenv)\.(exe|bat|cmd)$" | msr -aPA -e "(.+)"
+        call %~dp0\disable-exe-in-PATH.bat "\b(msbuild|devenv)\.(exe|bat|cmd)$" || exit /b -1
+    )
+    exit /b 0
+
+:Clear_Tmp_Envs
+    for /f "tokens=*" %%a in ('nin %1 nul "\bset\s+(?:/a\s+)?(\w+)=" --nt "\bset\s+.?PATH=" -iuPAC') do set %%a=
+    :: set | msr -t "VisualStudioVersion|NETFXSDKDir|UniversalCRTSdkDir|UCRTVersion|FSHARPINSTALLDIR|ExtensionSdkDir|WindowsSdk\w+|WindowsLib\w+|Framework\w+" -M -e "20\d{2}"
+    :: where msbuild.exe & where devenv.exe
+    call :Remove_Duplicates_in_PATH
+    REM msr -z "%PATH%" -t "\\*\s*;\s*" -o "\n" -aPAC | nin nul "(\S+.+)" -uipd -k2
+    REM msr -z "%PATH%" | msr -t "Input string.*" -o ""
+    exit /b 0
+
+:Remove_Duplicates_in_PATH
+    for /f "tokens=*" %%a in ('msr -z "%PATH%" -t "\\*\s*;\s*" -o "\n" -aPAC ^| msr -x \\ -o \ -aPAC ^| nin nul -uiPAC ^| msr -S -t "[\r\n]+(\S+)" -o ";$1" -aPAC ^| msr -S -t "\s+$" -o "" -aPAC') do set "PATH=%%a"
+
+    exit /b 0
--- disable-exe-in-PATH.bat
+++ disable-exe-in-PATH.bat
@@ -1,18 +1,17 @@
-::===============================================
-:: Find and disable specified exe files in PATH
-::===============================================
+::=============================================================
+:: Find and disable specified EXE or BAT/CMD files in in PATH.
+::
+:: More scripts: https://github.com/qualiu/msrTools
+::=============================================================
 @echo off
 
 SetLocal EnableExtensions EnableDelayedExpansion
 
 set msrExe=%~dp0msr.exe
-::if not exist %msrExe% powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri https://github.com/qualiu/msr/blob/master/tools/msr.exe?raw=true -OutFile %~dp0msr.exe"
-::if not exist %msrExe% powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/qualiu/msr/blob/master/tools/msr.exe?raw=true', '%~dp0msr.exe')"
 
 if not exist %msrExe% powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri https://github.com/qualiu/msr/blob/master/tools/msr.exe?raw=true -OutFile %~dp0msr.exe"
 
-%msrExe% -z "NotFirstArg%~1" -t "^NotFirstArg(|-h|--help|/\?)$" > nul
-if !ERRORLEVEL! NEQ 0 (
+%msrExe% -z "NotFirstArg%~1" -t "^NotFirstArg(|-h|--help|/\?)$" >nul || (
     echo Usage  : %~n0  ExeFilePattern     | %msrExe% -aPA -e "%~n0\s+(\S+).*"
     echo Example: %~n0  msr.exe            | %msrExe% -aPA -e "%~n0\s+(\S+).*"
     echo Example: %~n0  "^(msr|nin)\.exe$"       | %msrExe% -aPA -e "%~n0\s+(\S+).*"
@@ -24,18 +23,19 @@ set ninExe=%~dp0nin.exe
 if not exist %ninExe% powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri https://github.com/qualiu/msr/blob/master/tools/nin.exe?raw=true -OutFile %~dp0nin.exe"
 
 :: Display files with exe pattern %1
-%msrExe% -l -f "%~1" --wt --sz 2>nul -M -p "%PATH%"
-if %ERRORLEVEL% EQU 0 exit /b 0
+%msrExe% -l -f "%~1" --wt --sz 2>nul -M -p "%PATH%" && exit /b 0
+for /f "tokens=*" %%a in ('msr -t "[^\.\w-]+$" -o ";" -aPAC -z "%PATH%" ^| %msrExe% -x "\;" -o ";" -aPAC') do set "tmpPATH=%%a"
 
-for /f "tokens=*" %%a in ('msr -t "[^\.\w-]+$" -o ";" -aPAC -z "%PATH%" ^| msr -x "\;" -o ";" -aPAC') do set "PATH=%%a"
-set "tmpPATH=%PATH%"
-
-for /f "tokens=*" %%a in ('%msrExe% -l -f "%~1" -PAC 2^>nul -p "%PATH%" ^| %ninExe% nul "^([a-z]+.+?)[\\/][^\\/]*$" -iuPAC') do (
-    :: echo Will remove in PATH: %%a
-    for /f "tokens=*" %%b in ('%msrExe% -z "!tmpPATH!" -t "\\*\s*;\s*" -o "\n" -aPAC ^| %msrExe% --nx %%a -i -PAC ^| %msrExe% -S -t "[\r\n]+(\S+)" -o ";$1" -aPAC ^| %msrExe% -S -t "\s+$" -o "" -aPAC') do (
+for /f "tokens=*" %%a in ('%msrExe% -l -f "%~1" -PAC 2^>nul -p "%tmpPATH%" ^| %ninExe% nul "^([a-z]+.+?)[\\/][^\\/]*$" -iuPAC') do (
+    REM echo Will remove folder in PATH: "%%a" | %msrExe% -aPA -t "(.+)"
+    for /f "tokens=*" %%b in ('%msrExe% -z "!tmpPATH!" -t "\\*\s*;\s*" -o "\n" -aPAC ^| %msrExe% -x \\ -o \ -aPAC ^| %msrExe% --nx "%%a" -i -PAC ^| %ninExe% nul -uiPAC ^| %msrExe% -S -t "[\r\n]+(\S+)" -o ";$1" -aPAC ^| %msrExe% -S -t "\s+$" -o "" -aPAC') do (
+        REM Removed folder in PATH: "%%a" | %msrExe% -aPA -e "(.+)"
         set "tmpPATH=%%b"
     )
 )
 
+:: %~dp0msr.exe -l -f "%~1" --wt --sz 2>nul -M -p "!tmpPATH!"
 EndLocal & set "PATH=%tmpPATH%"
+:: %~dp0msr.exe -z "%PATH%" | %~dp0msr.exe -t "Input string.*" -o "" -M -e \d+ -c %0
+:: %~dp0msr.exe -z "%PATH%" -t "\\*\s*;\s*" -o "\n" -aPAC | %~dp0nin.exe nul "(\S+.+)" -uipd -k2 -c %0
 exit /b 0
